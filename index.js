74ffc4867012294bb8409141119441c3b1e6753d
Ron <ronmwangi00@gmail.com>
2023-06-08 10:38:28 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b){
+    return a+b;
+}
+console.log(add(3,5))
+
+function subtract(a,b){
+    return a-b;
+}
+console.log(add(3,5))
+
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(add(3,5))
+
+
+function divide(a,b){
+    return a/b;
+}
+console.log(add(3,5))
+
+function increment(n){
+    return n+1;
+}
+console.log(increment(n))
+
+function decrement(n){
+    return n-1;
+}
+console.log(decrement(n))
+
+function makeInt(n){
+    return parseInt(n, 10);
+
+}
+console.log(makeInt(n))
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal(n))
